468e637632f7cd5bea8fae726024d3a99349d8ce
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-05-01 23:33:59 +0430

    Add classify function

--- CA3/code.py
+++ CA3/code.py
@@ -1,10 +1,11 @@
+import math
 from collections import Counter
+from random import random
 
 import nltk
 import pandas as pd
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
-
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
@@ -19,18 +20,9 @@ def get_wordnet_pos(nltk_tag):
     }.get(nltk_tag[0].upper(), None)
 
 
-class Category:
-
-    def __init__(self, category_title, total_rows):
-        self.category_title = category_title
-        self.total_rows = total_rows
-        self.words = Counter()
-
-    def add_text(self, text):
-        for sent in nltk.sent_tokenize(text):
-            self.add_sentence(sent)
-
-    def add_sentence(self, sentence):
+def clean_text(text):
+    result = []
+    for sentence in nltk.sent_tokenize(text):
         tagged_sentence = [
             (word, get_wordnet_pos(tag))
             for (word, tag) in nltk.pos_tag(nltk.regexp_tokenize(sentence.lower(), r'\w+'))
@@ -43,7 +35,32 @@ class Category:
             else:
                 word = lemmatizer.lemmatize(word)
 
+            result.append(word)
+
+    return result
+
+
+class Category:
+
+    def __init__(self, category_title, category_total_rows, data_total_rows):
+        self.category_title = category_title
+        self.category_probability = math.log(
+            category_total_rows / data_total_rows, 10)
+
+        self.words = Counter()
+        self.word_count = 0
+
+    def add_text(self, text):
+        for word in clean_text(text):
             self.words[word] += 1
+            self.word_count += 1
+
+    def calculate_probability(self, text):
+        p = self.category_probability
+        for word in clean_text(text):
+            p += math.log(self.words[word] / self.word_count or 1e-6, 10)
+
+        return p
 
 
 class Classifier:
@@ -61,14 +78,38 @@ class Classifier:
 
         df = df.groupby(category_col)
 
-        self.data_categories = {}
+        self.categories = {}
+        self.test_data = {}
         for category_title in category_titles:
             category_df = df.get_group(category_title)
             category_total_rows, _ = category_df.shape
-            category = Category(category_title, category_total_rows)
+            category = Category(
+                category_title, category_total_rows, self.total_rows)
+
+            self.test_data[category_title] = []
 
             for index, row in category_df.iterrows():
-                for col in classification_cols:
-                    category.add_text(row[col])
+                if random() < 0.8:
+                    for col in classification_cols:
+                        category.add_text(row[col])
+                else:
+                    self.test_data[category_title].append(row)
 
-            self.data_categories[category_title] = category
+            self.categories[category_title] = category
+
+    def _find_category(self, text):
+        _, result_category = max([
+            (category.calculate_probability(text), category.category_title)
+            for category in self.categories.values()
+        ])
+
+        return result_category
+
+    def classify(self, test_file_name, classification_col):
+        df = pd.read_csv(test_file_name, usecols=[
+                         'index', classification_col]).dropna()
+
+        df[self._category_col] = df[classification_col].apply(
+            self._find_category)
+
+        return df

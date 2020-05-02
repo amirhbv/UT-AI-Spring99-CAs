4b4243911f840ca4f93978840d7bcf9fb8bc8eae
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-05-02 22:40:29 +0430

    Add evaluate and oversample

--- CA3/code.py
+++ CA3/code.py
@@ -6,6 +6,7 @@ import nltk
 import pandas as pd
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
+from prettytable import PrettyTable
 
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
@@ -44,8 +45,7 @@ class Category:
 
     def __init__(self, category_title, category_total_rows, data_total_rows):
         self.category_title = category_title
-        self.category_probability = math.log(
-            category_total_rows / data_total_rows, 10)
+        self.category_probability = category_total_rows / data_total_rows
 
         self.words = Counter()
         self.word_count = 0
@@ -56,17 +56,18 @@ class Category:
             self.word_count += 1
 
     def calculate_probability(self, text):
-        p = self.category_probability
+        p = math.log10(self.category_probability)
         for word in clean_text(text):
-            p += math.log(self.words[word] / self.word_count or 1e-6, 10)
+            p += math.log10((self.words[word] or 0.1) / self.word_count)
 
         return p
 
 
 class Classifier:
 
-    def __init__(self, data_file_name, classification_cols, category_col):
+    def __init__(self, data_file_name, classification_cols, category_col, oversample=False):
         self._category_col = category_col
+        self._classification_cols = classification_cols
 
         df = pd.read_csv(data_file_name,
                          usecols=[*classification_cols, category_col])
@@ -77,33 +78,131 @@ class Classifier:
         category_titles = df[category_col].unique()
 
         df = df.groupby(category_col)
+        max_category_data_rows = max([
+            categpry_data_rows
+            for categpry_data_rows, _ in [
+                category.shape for _, category in df
+            ]
+        ])
+        print(max_category_data_rows)
 
         self.categories = {}
         self.test_data = {}
         for category_title in category_titles:
             category_df = df.get_group(category_title)
             category_total_rows, _ = category_df.shape
+
+            if oversample:
+                category_df = category_df.append(
+                    category_df.sample(
+                        n=max_category_data_rows - category_total_rows,
+                        replace=True,
+                    )
+                )
+
+            category_total_rows, _ = category_df.shape
             category = Category(
                 category_title, category_total_rows, self.total_rows)
 
-            self.test_data[category_title] = []
-
-            for index, row in category_df.iterrows():
+            test_data = []
+            for _, row in category_df.iterrows():
                 if random() < 0.8:
                     for col in classification_cols:
                         category.add_text(row[col])
                 else:
-                    self.test_data[category_title].append(row)
+                    test_data.append(row)
+
+            self.test_data[category_title] = pd.DataFrame(
+                columns=category_df.columns,
+                data=test_data,
+            )
 
             self.categories[category_title] = category
 
-    def _find_category(self, text):
+    def _find_category(self, text, include_categories=None):
         _, result_category = max([
             (category.calculate_probability(text), category.category_title)
             for category in self.categories.values()
+            if not include_categories or category.category_title in include_categories
         ])
 
         return result_category
+
+    def evaluate(self, classification_col, categories=None):
+        valid_categories = list(self.categories.keys())
+        if not categories:
+            categories = valid_categories
+        else:
+            categories = [
+                category for category in categories if category in valid_categories]
+
+        actual_categories, predicted_categories = zip(*[
+            (category, self._find_category(
+                row[classification_col], categories))
+            for category in categories
+            for _, row in self.test_data[category].iterrows()
+        ])
+
+        category_indices = {
+            category: index for (index, category) in enumerate(categories)
+        }
+
+        confustion_matrix = [[0 for _ in categories] for _ in categories]
+        for actual_category, predicted_category in zip(actual_categories, predicted_categories):
+            confustion_matrix[
+                category_indices[actual_category]
+            ][
+                category_indices[predicted_category]
+            ] += 1
+
+        confustion_matrix_table = PrettyTable(
+            field_names=[
+                'Confusion Matrix',
+                *categories
+            ],
+        )
+        for i, row in enumerate(confustion_matrix):
+            confustion_matrix_table.add_row([
+                categories[i],
+                *row,
+            ])
+
+        print(confustion_matrix_table)
+
+        metrics_table = PrettyTable(
+            field_names=[
+                '',
+                'Accuracy',
+                'Precision',
+                'Recall',
+            ],
+        )
+
+        all_true_positive_cases = sum(
+            confustion_matrix[i][i] for i in range(len(confustion_matrix)))
+        all_cases = len(actual_categories)
+        accuracy = all_true_positive_cases / all_cases
+
+        for i, category in enumerate(categories):
+            true_positive_cases = confustion_matrix[i][i]
+            actual_positive_cases = sum(
+                confustion_matrix[i][j] for j in range(len(confustion_matrix)))
+            predicted_positive_cases = sum(
+                confustion_matrix[j][i] for j in range(len(confustion_matrix)))
+
+            precision = true_positive_cases / predicted_positive_cases
+            recall = true_positive_cases / actual_positive_cases
+
+            metrics_table.add_row([
+                category,
+                accuracy,
+                precision,
+                recall,
+            ])
+
+        print(metrics_table)
+
+        return confustion_matrix
 
     def classify(self, test_file_name, classification_col):
         df = pd.read_csv(test_file_name, usecols=[
--- CA3/test.py
+++ CA3/test.py
@@ -1,7 +1,39 @@
 from code import Classifier
+from time import time
+import pandas as pd
 
+start = time()
 classifier = Classifier(
     data_file_name='./data.csv',
     classification_cols=['short_description', 'headline'],
     category_col='category',
+    oversample=True,
 )
+print("Elapsed Time (train):", time() - start)
+
+start = time()
+confustion_matrix = classifier.evaluate(
+    classification_col='short_description',
+    categories=["TRAVEL", "BUSINESS"],
+)
+print("Elapsed Time (evaluate phase1):", time() - start)
+
+start = time()
+confustion_matrix = classifier.evaluate(
+    classification_col='short_description',
+)
+print("Elapsed Time (evaluate phase2):", time() - start)
+
+start = time()
+res = classifier.classify(
+    test_file_name='./test.csv',
+    classification_col='short_description',
+)
+print("Elapsed Time (classify):", time() - start)
+
+res[['index', 'category']].to_csv('output.csv', index=False)
+pd1 = pd.read_csv('output.csv')
+pd2 = pd.read_csv('ans.csv')
+joined = pd.merge(pd1, pd2, on='index')
+joined = joined[joined['category_x'] != joined['category_y']]
+print(joined.shape)
